ffdd57941c3b65cd8956e9c2120af78cd4f74499
Arturo058 <24103003@alumnos.univo.edu.mx>
2025-06-14 01:00:35 -0600

    Delete ACT_NUMERO_6 directory

--- ACT_NUMERO_6/Main.java
+++
@@ -1,8 +0,0 @@
-package ACT_NUMERO_6;
-
-public class Main {
-    public static void main(String[] args) {
-        Biblioteca biblioteca = new Biblioteca();
-        biblioteca.mostrarMenu();
-    }
-}
