0c3cd87329d7aeac27115c553dbbc18295d34dcf
Arturo058 <24103003@alumnos.univo.edu.mx>
2025-06-14 01:27:33 -0600

    Se creo la clase Main

---
+++ ACT_NUMERO_6/Main.java
@@ -0,0 +1,8 @@
+package ACT_NUMERO_6;
+
+public class Main {
+    public static void main(String[] args) {
+        Biblioteca biblioteca = new Biblioteca();
+        biblioteca.mostrarMenu();
+    }
+}
